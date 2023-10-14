4db0ccbac59e51edb34fbb8b6c3529dcadc83699
yujiromorota07 <yujiromorota07@gmail.com>
2023-10-15 03:08:03 +0900

    環境変数でdb接続

--- index.ts
+++ index.ts
@@ -1,10 +1,30 @@
 import express from "express";
+import mysql, { ConnectionOptions } from "mysql2/promise";
+import dotenv from "dotenv";
 
 const app = express();
 const PORT = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express with TypeScript!");
+dotenv.config();
+
+const dbConfig: ConnectionOptions = {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  port: parseInt(process.env.DB_PORT || "3306", 10), // 数値に変換し、デフォルト値を指定
+};
+
+app.get("/", async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(dbConfig);
+    await connection.ping();
+    connection.end();
+    res.send("MySQL connection successful!");
+  } catch (error: any) {
+    res.status(500).send("Failed to connect to MySQL: " + error.message);
+  }
+  // res.send("Hello, Express with TypeScript!");
 });
 
 app.listen(PORT, () => {
